79267f140520cb76536d504e46e610d131149500
yangguoqian0716 <105093945+yangguoqian0716@users.noreply.github.com>
2024-06-17 09:19:06 +0800

    Update my_strcpy.c

--- my_strcpy.c
+++ my_strcpy.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
-char* my_strcpy(char *dest,char *src)
+void my_strcpy(char *dest,char *src)
 {
-  while((*dest++==*src++)!='\0');
-
-  return dest
+  while((*dest++=*src++)!='\0');
 }
+
+int main()
+{
+  char source[] = "Hello World!";
+  char destination[50];
+
+  my_strcpy(destination,source);
+
+  printf("source str: %s\n",source);
+  printf("destination str: %s\n",destionation);
+
+  return 0;
+}
+
 
